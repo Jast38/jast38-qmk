d211a9a98f5b68ea86b05261040e60c52a2f09a8
Jannick Stuby <mail@jannick-stuby.com>
2024-03-21 14:26:49 +0100

    updated display

--- keyboards/bastardkb/jast_dilemma/4x6_4/keymaps/via/rules.mk
+++ keyboards/bastardkb/jast_dilemma/4x6_4/keymaps/via/rules.mk
@@ -4,4 +4,4 @@ ENCODER_MAP_ENABLE = yes
 QUANTUM_PAINTER_ENABLE = yes
 QUANTUM_PAINTER_DRIVERS += gc9a01_spi
 
-SRC += generated/SRC += generated/totoro-jumping.qgf.c
+SRC += generated/totoro-jumping.qgf.c
